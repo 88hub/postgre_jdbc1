f13e8003ceda920b5d9d70fb9cf107586b595e6e
ubu ubu <88dmitriev@gmail.com>
2023-01-30 11:53:34 +0500

    1

--- src/main/java/org/dmtv/postgre_jdbc/Main.java
+++ src/main/java/org/dmtv/postgre_jdbc/Main.java
@@ -11,7 +11,7 @@ public class Main {
             e.printStackTrace();
         }
 
-        insertMethod();
+        insertMethod("myUser123", "311415", "test@test.com");
         selectMethod();
 
     }
@@ -23,10 +23,13 @@ public class Main {
                     "postgres", "postgrespassword");
 
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT login FROM table1");
+            String query = "SELECT t1.login AS login1, t2.login as login2 " +
+                    "FROM table1 t1 JOIN table2 t2 ON t1.login = t2.login";
+            ResultSet resultSet = statement.executeQuery(query);
 
             while (resultSet.next()) {
-                System.out.println(resultSet.getString("login"));
+                System.out.println(resultSet.getString("login1"));
+                System.out.println(resultSet.getString("login2"));
             }
             statement.close();
         } catch (SQLException e) {
@@ -42,18 +45,23 @@ public class Main {
         }
     }
 
-    public static void insertMethod() {
+    public static void insertMethod(String login, String password, String mail) {
         String sqlLine = "INSERT INTO table1 (login, password, date) VALUES (?, ?, ?)";
-
+        String sqlLine2 = "INSERT INTO table2 (login, email) VALUES (?, ?)";
         try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5433/mydb",
                 "postgres", "postgrespassword");) {
             PreparedStatement preparedStatement = connection.prepareStatement(sqlLine);
-            int randNumUser = (int)(Math.random() * 10000);
-            preparedStatement.setString(1, "newUser" + randNumUser);
-            preparedStatement.setString(2,"newPassword" + randNumUser);
+            preparedStatement.setString(1, login);
+            preparedStatement.setString(2,password);
             preparedStatement.setDate(3, new Date(System.currentTimeMillis()));
             preparedStatement.executeUpdate();
             preparedStatement.close();
+
+            PreparedStatement preparedStatement2 = connection.prepareStatement(sqlLine2);
+            preparedStatement2.setString(1, login);
+            preparedStatement2.setString(2, mail);
+            preparedStatement2.executeUpdate();
+            preparedStatement2.close();
         }
         catch (SQLException e) {
             e.printStackTrace();
